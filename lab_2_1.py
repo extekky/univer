057f32d7d25ec9fe9db20f9846da578c0824a867
stas <st.stanislove@yandex.ru>
2024-03-02 18:59:11 +0300

    fix lab_2_1.py

--- lab_2_1.py
+++ lab_2_1.py
@@ -1,66 +1,59 @@
-# Пример использования stas@ubuntu:~$ python3 /home/stas/wtf.py /home/stas/Downloads
-import json
-import stat
-import sys
 import os
-import pwd
+import sys
+import stat
+import json
 
 
-def type_entity(file_path):
-    file_stat = os.stat(file_path)
-    file_mode = file_stat.st_mode
-
-    if stat.S_ISREG(file_mode):
-        return "normal"
-    elif stat.S_ISDIR(file_mode):
-        return "dir"
-    elif stat.S_ISCHR(file_mode):
-        return "char"
-    elif stat.S_ISBLK(file_mode):
-        return "block"
-    else:
-        return "unknown"
+def getTypeFile(MY_FILE_PATH) -> str:
+    if stat.S_ISCHR(os.stat(MY_FILE_PATH).st_mode):
+        return 'char'
+    if stat.S_ISDIR(os.stat(MY_FILE_PATH).st_mode):
+        return 'dir'
+    if stat.S_ISBLK(os.stat(MY_FILE_PATH).st_mode):
+        return 'block'
+    if stat.S_ISREG(os.stat(MY_FILE_PATH).st_mode):
+        return 'normal'
 
 
-def get_structure_data(my_path):
-    file_tree = {
-        'type': type_entity(my_path),
-        'name': os.path.basename(my_path),
+def makeJsonTree(MY_FILE_PATH: str) -> dict:
+    dir_name, base_name = os.path.split(MY_FILE_PATH)
+    if not base_name:
+        base_name = os.path.basename(dir_name)
+
+    tree = {
+        'name': base_name,
+        'type': getTypeFile(MY_FILE_PATH),
         'children': []
     }
-    for entity in os.scandir(my_path):
-        if entity.is_file():
-            file_tree['children'].append({
-                'type': type_entity(entity),
-                'name': os.path.basename(entity),
-                'size': os.path.getsize(entity)
+
+    for what_is_that in os.scandir(MY_FILE_PATH):
+        if getTypeFile(what_is_that.path) in ('char', 'normal', 'block'):
+            tree['children'].append({
+                'name': os.path.basename(what_is_that.path),
+                'type': getTypeFile(what_is_that.path),
+                'size': os.path.getsize(what_is_that.path)
             })
 
-        if entity.is_dir():
-            file_tree["children"].append(get_structure_data(entity.path))
+        if getTypeFile(what_is_that.path) == 'dir':
+            tree['children'].append(makeJsonTree(what_is_that.path))
 
-    return file_tree
-
-
-def write():
-    json_data = json.dumps(get_structure_data(path), indent=3)
-    print(json_data)
-
-    current_user = pwd.getpwuid(os.getuid())
-    username = current_user.pw_name
-
-    output_file_path = f'/home/{username}/output.json'
-    with open(output_file_path, 'w') as output_file:
-        output_file.write(json_data)
-
-    print('\nФайл сохранен по адресу:', output_file_path)
+    return tree
 
 
-if len(sys.argv) < 2:
-    print("Пример использования\nstas@ubuntu:~$ python3 /home/stas/wtf.py /home/stas/Downloads")
-else:
-    path = sys.argv[1]
-    if not os.path.isdir(path):
-        print("Invalid path")
-    else:
-        write()
+def getReadbleJsonTree(JSON_TREE) -> str:
+    return json.dumps(JSON_TREE, indent=4)
+
+
+def main():
+    if len(sys.argv) != 2:
+        return "Usage: python3 <python_file> <directory_path>"
+
+    directory_path = sys.argv[1]
+    if not os.path.isdir(directory_path):
+        return f"Invalid path: {directory_path}"
+
+    return getReadbleJsonTree(makeJsonTree(directory_path))
+
+
+if __name__ == "__main__":
+    print(main())
