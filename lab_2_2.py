0b08a07f41cee549cc7841855b0fed7a7a6efcfb
stas <st.stanislove@yandex.ru>
2024-03-03 15:08:03 +0300

    fix lab_2_2.py

--- lab_2_2.py
+++ lab_2_2.py
@@ -3,15 +3,30 @@ import json
 import evdev
 
 
-def read_input_events(input_device):
+def readInputEvents(input_device):
     events = []
-    for event in input_device.read_loop():
-        if event.type == evdev.ecodes.EV_KEY:
-            events.append({'type': 'EV_KEY', 'code': event.code, 'value': event.value})
-        elif event.type == evdev.ecodes.EV_REL:
-            events.append({'type': 'EV_REL', 'code': event.code, 'value': event.value})
-        if len(events) >= 10:
-            return events
+    try:
+        for event in input_device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                event_dict = {
+                    "type": "EV_KEY",
+                    "code": event.code,
+                    "state": event.value
+                }
+                events.append(event_dict)
+                print(event_dict)
+            if event.type == evdev.ecodes.EV_REL:
+                event_dict = {
+                    "type": "EV_REL",
+                    "value": event.value
+                }
+                events.append(event_dict)
+                print(event_dict)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        input_device.close()
+        return events
 
 
 def main():
@@ -23,10 +38,13 @@ def main():
         input_device = evdev.InputDevice(input_device_path)
     except FileNotFoundError:
         return f"Error: Input device '{input_device_path}' not found"
+    except PermissionError:
+        return f"Error: Permission denied to access input device '{input_device_path}'"
 
-    events = read_input_events(input_device)
+    events = readInputEvents(input_device)
     return json.dumps(events, indent=4)
 
 
 if __name__ == "__main__":
     print(main())
+
