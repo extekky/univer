d62b682fad1dcdeeb9b55d46b1a5d9726a3e4e94
stas <st.stanislove@yandex.ru>
2024-03-06 19:00:05 +0300

    added last argument 'file'

--- lab_3.py
+++ lab_3.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import argparse  # The argparse module’s support for command-line
                  # interfaces is built around an instance of class named argparse.ArgumentParser.
@@ -33,8 +34,11 @@ def parseInfo() -> argparse.Namespace:
     # What positions should be display if there are any           (Only one argument, but can be called many times)
     parse.add_argument('-i', '--itm', dest='pos', nargs=1, action='extend', type=int)
 
-    # Unlimited number of positional arguments
+    # Unlimited number of positional arguments                                          (Unlimited number of items)
     parse.add_argument('massive',  nargs='*', action='extend', type=int)
+
+    # Output the contents of a file or input stream                                             (Only one argument)
+    parse.add_argument('-f', '--file',  type=argparse.FileType('r'))
 
     return parse.parse_args()  # The parse_args() method runs the parser and
                                # places the extracted data in a argparse.Namespace object.
@@ -44,7 +48,8 @@ def parseInfo() -> argparse.Namespace:
 def main():
     json_document = {}  # Object for storing the structure of a json document
 
-    incoming_info = parseInfo()  # The incoming information will be processed by the parser and transmitted to incoming_info
+    incoming_info = parseInfo()  # The incoming information will be processed
+                                 # by the parser and transmitted to incoming_info
 
     if incoming_info.pid is True:  # If the --pid argument was caught
         json_document['pid'] = os.getpid()  # add it to the json document
@@ -72,6 +77,17 @@ def main():
             value for i, value in enumerate(incoming_info.massive)
             if i in incoming_info.pos
         ]
+
+    if incoming_info.file is not None:
+        if incoming_info.file.name == '<stdin>':
+            from_stdin = []
+            try:
+                while True:
+                    from_stdin.append(sys.stdin.readline().strip())
+            except KeyboardInterrupt:
+                json_document['file'] = from_stdin
+        else:
+            json_document['file'] = [line.strip() for line in incoming_info.file.readlines() if line.strip()]
 
     return getReadbleJsonTree(json_document)
 
