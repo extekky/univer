bdb4914fb38ab6faeaa9177479194cf42f9d8e8a
stas <st.stanislove@yandex.ru>
2024-05-24 13:48:16 +0300

    добавление лабы 6

--- lab_6_1.py
+++ lab_6_1.py
@@ -5,21 +5,23 @@ import socket
 server = "djxmmx.net"
 port = 17
 
+
 def main():
     # Получение IP-адреса сервера
     server_ip = socket.gethostbyname(server)
-    
-    # Создание UDP-пакета
-    packet = IP(dst=server_ip)/UDP(dport=port)/Raw(load="")
-    
+
+    # Создание UDP-пакета c пустй=ой полкезной нагрузкой
+    packet = IP(dst=server_ip) / UDP(dport=port) / Raw(load="")
+
     # Отправка пакета и ожидание ответа
     ans, unans = sr(packet, timeout=2, multi=True)
-    
+
     # Обработка ответа
     for snd, rcv in ans:
         # Извлечение и декодирование строки ответа
         quote = rcv[Raw].load.decode('utf-8')
         print(quote)
+
 
 if __name__ == "__main__":
     main()
